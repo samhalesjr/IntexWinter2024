df3f395531f0328df425666b059e9e126d8c4182
samhalesjr <samhalesjr@gmail.com>
2024-04-10 12:08:11 -0600

    Fixed db issue

--- IntexWinter2024/Controllers/HomeController.cs
+++ IntexWinter2024/Controllers/HomeController.cs
@@ -17,49 +17,49 @@ namespace IntexWinter2024.Controllers
             _context = context;
         }
 
-        //public IActionResult Index()
-        //{
-        //    var products = _context.Products.ToList(); // Retrieve all products from the database
-        //    return View(products); // Pass the products to the view
-        //}
-
         public IActionResult Index()
         {
-            string filePath = @"C:\Users\samha\Downloads\INTEX W24 Dataset - LineItems.csv"; // Update this to your CSV file path
-
-            using (TextFieldParser csvParser = new TextFieldParser(filePath))
-            {
-                csvParser.TextFieldType = FieldType.Delimited;
-                csvParser.SetDelimiters(",");
-                csvParser.HasFieldsEnclosedInQuotes = true;
-
-                // Skip the header row
-                csvParser.ReadLine();
-
-                while (!csvParser.EndOfData)
-                {
-                    string[] fields = csvParser.ReadFields();
-
-                    //Console.WriteLine($"Adding Order: {fields[0]} \t 1:{fields[1]} \t 1:{fields[2]} \t 1:{fields[3]} \t 1:{fields[4]} \t 1:{fields[5]} \t 1:{fields[6]} \t 1:{fields[7]} \t 1:{fields[8]} \t 1:{fields[9]} \t 1:{fields[10]} \t 1:{fields[11]} \t 1:{fields[12]}");
-
-                    var order = new ProductCategory
-                    {
-                        // Adjust these to match the order and format of your CSV
-                        ProductId = int.Parse(fields[0]),
-                        CategoryName = fields[1]
-                    };
-
-                    // Optional: For debugging, printing the added order's info
-                    //Console.WriteLine($"Adding Order Amount: {order.Amount} - Date: {order.Date} - Fraud: {order.Fraud}");
-
-                    _context.ProductCategories.Add(order);
-                }
-
-                _context.SaveChanges(); // Saves all added orders to the database
-            }
-
-            return View();
+            var products = _context.Products.ToList(); // Retrieve all products from the database
+            return View(products); // Pass the products to the view
         }
+
+        //public IActionResult Index()
+        //{
+        //    string filePath = @"C:\Users\samha\Downloads\product_categories - product_categories.csv"; // Update this to your CSV file path
+
+        //    using (TextFieldParser csvParser = new TextFieldParser(filePath))
+        //    {
+        //        csvParser.TextFieldType = FieldType.Delimited;
+        //        csvParser.SetDelimiters(",");
+        //        csvParser.HasFieldsEnclosedInQuotes = true;
+
+        //        // Skip the header row
+        //        csvParser.ReadLine();
+
+        //        while (!csvParser.EndOfData)
+        //        {
+        //            string[] fields = csvParser.ReadFields();
+
+        //            //Console.WriteLine($"Adding Order: {fields[0]} \t 1:{fields[1]} \t 1:{fields[2]} \t 1:{fields[3]} \t 1:{fields[4]} \t 1:{fields[5]} \t 1:{fields[6]} \t 1:{fields[7]} \t 1:{fields[8]} \t 1:{fields[9]} \t 1:{fields[10]} \t 1:{fields[11]} \t 1:{fields[12]}");
+
+        //            var order = new ProductCategory
+        //            {
+        //                // Adjust these to match the order and format of your CSV
+        //                ProductId = int.Parse(fields[0]),
+        //                CategoryName = fields[1]
+        //            };
+
+        //            // Optional: For debugging, printing the added order's info
+        //            //Console.WriteLine($"Adding Order Amount: {order.Amount} - Date: {order.Date} - Fraud: {order.Fraud}");
+
+        //            _context.ProductCategories.Add(order);
+        //        }
+
+        //        _context.SaveChanges(); // Saves all added orders to the database
+        //    }
+
+        //    return View();
+        //}
 
 
         public IActionResult Privacy()
---
+++ IntexWinter2024/Migrations/20240410180543_renamedproductcatetable.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IntexWinter2024.Migrations
+{
+    /// <inheritdoc />
+    public partial class renamedproductcatetable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_Role_RoleId",
+                table: "Customers");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Role",
+                table: "Role");
+
+            migrationBuilder.RenameTable(
+                name: "Role",
+                newName: "Roles");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Roles",
+                table: "Roles",
+                column: "RoleId");
+
+            migrationBuilder.CreateTable(
+                name: "ProductCategories",
+                columns: table => new
+                {
+                    ProductCategoryId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductCategories", x => x.ProductCategoryId);
+                    table.ForeignKey(
+                        name: "FK_ProductCategories_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductCategories_ProductId",
+                table: "ProductCategories",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_Roles_RoleId",
+                table: "Customers",
+                column: "RoleId",
+                principalTable: "Roles",
+                principalColumn: "RoleId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_Roles_RoleId",
+                table: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "ProductCategories");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Roles",
+                table: "Roles");
+
+            migrationBuilder.RenameTable(
+                name: "Roles",
+                newName: "Role");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Role",
+                table: "Role",
+                column: "RoleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_Role_RoleId",
+                table: "Customers",
+                column: "RoleId",
+                principalTable: "Role",
+                principalColumn: "RoleId");
+        }
+    }
+}
