35a0c54d8cce7a9fa622addda1d1bd0510e4cbfd
samhalesjr <samhalesjr@gmail.com>
2024-04-10 13:35:15 -0600

    AddedProdBasedRecommendations

---
+++ IntexWinter2024/Migrations/20240410192108_AddedProdRecTable.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IntexWinter2024.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedProdRecTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProductBasedProductRecommendations",
+                columns: table => new
+                {
+                    ProductBasedProductRecommendationId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    RecommendationOneProductId = table.Column<int>(type: "int", nullable: false),
+                    RecommendationTwoProductId = table.Column<int>(type: "int", nullable: false),
+                    RecommendationThreeProductId = table.Column<int>(type: "int", nullable: false),
+                    RecommendationFourProductId = table.Column<int>(type: "int", nullable: false),
+                    RecommendationFiveProductId = table.Column<int>(type: "int", nullable: false),
+                    RecommendationSixProductId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductBasedProductRecommendations", x => x.ProductBasedProductRecommendationId);
+                    table.ForeignKey(
+                        name: "FK_ProductBasedProductRecommendations_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductBasedProductRecommendations_Products_RecommendationFiveProductId",
+                        column: x => x.RecommendationFiveProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_ProductBasedProductRecommendations_Products_RecommendationFourProductId",
+                        column: x => x.RecommendationFourProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_ProductBasedProductRecommendations_Products_RecommendationOneProductId",
+                        column: x => x.RecommendationOneProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_ProductBasedProductRecommendations_Products_RecommendationSixProductId",
+                        column: x => x.RecommendationSixProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_ProductBasedProductRecommendations_Products_RecommendationThreeProductId",
+                        column: x => x.RecommendationThreeProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_ProductBasedProductRecommendations_Products_RecommendationTwoProductId",
+                        column: x => x.RecommendationTwoProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductBasedProductRecommendations_ProductId",
+                table: "ProductBasedProductRecommendations",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductBasedProductRecommendations_RecommendationFiveProductId",
+                table: "ProductBasedProductRecommendations",
+                column: "RecommendationFiveProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductBasedProductRecommendations_RecommendationFourProductId",
+                table: "ProductBasedProductRecommendations",
+                column: "RecommendationFourProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductBasedProductRecommendations_RecommendationOneProductId",
+                table: "ProductBasedProductRecommendations",
+                column: "RecommendationOneProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductBasedProductRecommendations_RecommendationSixProductId",
+                table: "ProductBasedProductRecommendations",
+                column: "RecommendationSixProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductBasedProductRecommendations_RecommendationThreeProductId",
+                table: "ProductBasedProductRecommendations",
+                column: "RecommendationThreeProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductBasedProductRecommendations_RecommendationTwoProductId",
+                table: "ProductBasedProductRecommendations",
+                column: "RecommendationTwoProductId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductBasedProductRecommendations");
+        }
+    }
+}
---
+++ IntexWinter2024/Models/ProductBasedProductRecommendation.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntexWinter2024.Models
+{
+    public class ProductBasedProductRecommendation
+    {
+        [Key]
+        public int ProductBasedProductRecommendationId { get; set; }
+
+        [ForeignKey("Product")]
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+
+        [ForeignKey("RecommendationOne")]
+        public int RecommendationOneProductId { get; set; }
+        public virtual Product RecommendationOne { get; set; }
+
+        [ForeignKey("RecommendationTwo")]
+        public int RecommendationTwoProductId { get; set; }
+        public virtual Product RecommendationTwo { get; set; }
+
+        [ForeignKey("RecommendationThree")]
+        public int RecommendationThreeProductId { get; set; }
+        public virtual Product RecommendationThree { get; set; }
+
+        [ForeignKey("RecommendationFour")]
+        public int RecommendationFourProductId { get; set; }
+        public virtual Product RecommendationFour { get; set; }
+
+        [ForeignKey("RecommendationFive")]
+        public int RecommendationFiveProductId { get; set; }
+        public virtual Product RecommendationFive { get; set; }
+
+        [ForeignKey("RecommendationSix")]
+        public int RecommendationSixProductId { get; set; }
+        public virtual Product RecommendationSix { get; set; }
+    }
+}
