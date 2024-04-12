9432130fd83df0d215390117d78e2e99f242495c
samhalesjr <samhalesjr@gmail.com>
2024-04-11 18:11:58 -0600

    Added temp cust column

--- IntexWinter2024/Migrations/20240411231339_FixedCustMaybe.Designer.cs
+++ IntexWinter2024/Migrations/20240411231339_FixedCustMaybe.Designer.cs
@@ -4,6 +4,7 @@ using IntexWinter2024.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace IntexWinter2024.Migrations
 {
     [DbContext(typeof(IntexWinter2024Context))]
-    partial class IntexWinter2024ContextModelSnapshot : ModelSnapshot
+    [Migration("20240411231339_FixedCustMaybe")]
+    partial class FixedCustMaybe
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
